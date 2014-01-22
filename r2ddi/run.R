78ddf90a6db83e53e40b872bbd6fd81faac91ce7
mhebing <marcel@ddionrails.org>
2014-01-22 21:07:06 +0100

    Update r2ddi task

--- r2ddi/run.R
+++ r2ddi/run.R
@@ -1,6 +1,6 @@
 
 library("r2ddi")
 
-dir2xml(path_in = "../data/04-dist/german/Stata12/", path_out = "de/")
-dir2xml(path_in = "../data/04-dist/english/Stata12/", path_out = "en/")
+dir2xml(path_in = "../data/02-dist/de/", path_out = "de/")
+dir2xml(path_in = "../data/02-dist/en/", path_out = "en/")
 
