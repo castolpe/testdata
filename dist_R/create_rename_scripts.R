e2bc05e7ea44473c7f5a8566a961b1901ca235e3
mhebing <marcel@ddionrails.org>
2014-09-09 11:09:14 +0200

    Minor update

--- dist_R/create_rename_scripts.R
+++ dist_R/create_rename_scripts.R
@@ -20,19 +20,20 @@ library("foreign")
     x <- filenames[i, ]
     raw  <- names(read.dta(x$raw))
     dist <- names(read.dta(x$dist))
-    .write_script(raw, dist, paste("dist_script/", x$name, ".do", sep=""))
-    .write_script(dist, raw, paste("dist_back/", x$name, ".do", sep=""))
+    .write_script(raw, dist, x$raw, x$dist, paste("dist_script/", x$name, ".do", sep=""))
+    .write_script(dist, raw, x$dist, x$raw, paste("dist_back/", x$name, ".do", sep=""))
   }
 }
 
-.write_script <- function(names1, names2, fileout){
+.write_script <- function(names1, names2, file1, file2, fileout){
   if(lenght(names1) != length(names2)) {
     cat("[ERROR] Names do not match.")
     return(nil)
   }
-  script <- "\n"
+  script <- paste("use", file1, ", clear \n", sep=" ")
   for(i in seq(lenght(names1)))
   script <- paste(script, "rename", names1[i], names2[i], "\n", sep=" ")
+  script <- paste(script, "saveold", file2, "\n", sep=" ")
   write(script, fileout)
 }
 
